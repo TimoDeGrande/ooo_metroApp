490c21b1578be9f735896b3276e1d6da97695389
TimoDeGrande <timo.degrande@student.ucll.be>
2022-12-27 14:25:59 +0100

    added gates

--- src/view/MetroStationView.java
+++ src/view/MetroStationView.java
@@ -4,10 +4,13 @@ import domain.controller.MetroStationViewController;
 import javafx.collections.FXCollections;
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
-import sun.security.util.PendingException;
 
 import java.util.ArrayList;
 
@@ -32,13 +35,28 @@ public class MetroStationView {
 	}
 
 	public void updateIdCheckbox(ArrayList<Integer> metroCardIds) {
-		ChoiceBox choiceBox = new ChoiceBox();
-		choiceBox.setItems(FXCollections.observableArrayList(metroCardIds));
-		Group root = new Group();
-		root.getChildren().add(choiceBox);
+		GridPane root = new GridPane();
+		root = startMetroSim(root, metroCardIds);
 		Scene scene = new Scene(root, 650, 300);
 		this.stage.setScene(scene);
 		stage.sizeToScene();
 		stage.show();
+	}
+
+	public GridPane startMetroSim(GridPane root, ArrayList<Integer> metroCardIds){
+		for(int i = 0; i < 4; i++){
+			ChoiceBox test = new ChoiceBox();
+			test.setItems(FXCollections.observableArrayList(metroCardIds));
+			VBox box = new VBox();
+			Text text = new Text(String.format("Gate %s", i + 1));
+			Button scan = new Button("scan");
+			Button walkgate = new Button("Walk through gate");
+			Text error = new Text();
+			scan.setOnAction(event -> error.setText("pressed scan"));
+			walkgate.setOnAction(event -> error.setText("pressed walk through gate"));
+			box.getChildren().addAll(text, test, scan, walkgate, error);
+			root.add(box, i,0);
+		}
+		return root;
 	}
 }
