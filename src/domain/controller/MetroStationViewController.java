b82730251ab89a6146378db10c7c1e3fa15e2d4f
TimoDeGrande <timo.degrande@student.ucll.be>
2022-12-28 16:22:54 +0100

    het werkt!

--- src/domain/controller/MetroStationViewController.java
+++ src/domain/controller/MetroStationViewController.java
@@ -23,7 +23,9 @@ public class MetroStationViewController implements Observer {
     @Override
     public void update(MetroEventsEnum e) {
         ArrayList<Integer> ids = this.facade.getMetroCardIdList();
-        this.view.updateIdCheckbox(ids, this.facade.getMetroGateAmount());
+        this.view.updateIdCheckbox(ids);
+        //this.view.updateGates(this.facade.getMetroGateAmount());
+        this.view.updateGates(4);
     }
 
     @Override
--- src/view/MetroStationView.java
+++ src/view/MetroStationView.java
@@ -5,6 +5,7 @@ import javafx.collections.FXCollections;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
@@ -16,32 +17,60 @@ import java.util.ArrayList;
 
 public class MetroStationView {
 	private MetroStationViewController controller;
-	private Stage stage = new Stage();		
-	
-	public MetroStationView(){			
+	private Stage stage = new Stage();
+	private ChoiceBox ids = new ChoiceBox();
+	private GridPane root = new GridPane();
+	private int gates = 0;
+
+	public MetroStationView(){
 		stage.setTitle("METRO STATION VIEW");
 		stage.initStyle(StageStyle.UTILITY);
 		stage.setX(5);
 		stage.setY(390);
-		Group root = new Group();
-		Scene scene = new Scene(root, 650, 300);			
+		Scene scene = new Scene(this.root, 650, 300);
 		stage.setScene(scene);
-		stage.sizeToScene();			
-		stage.show();		
+		stage.sizeToScene();
+		stage.show();
 	}
 
 	public void setController(MetroStationViewController metroStationViewController) {
 		this.controller = metroStationViewController;
 	}
 
-	public void updateIdCheckbox(ArrayList<Integer> metroCardIds, int gates) {
-		GridPane root = new GridPane();
-		root = startMetroSim(root, metroCardIds, gates);
-		Scene scene = new Scene(root, 650, 300);
-		this.stage.setScene(scene);
+	public void updateIdCheckbox(ArrayList<Integer> metroCardIds) {
+		ChoiceBox choiceBox = new ChoiceBox();
+		choiceBox.setItems(FXCollections.observableArrayList(metroCardIds));
+		this.ids = choiceBox;
+		this.updateView();
+	}
+
+	public void updateGates(int gates) {
+		this.gates = gates;
+		this.updateView();
+	}
+
+	public void updateView(){
+		GridPane newRoot = new GridPane();
+		for(int i = 0; i < this.gates; i++){
+			ChoiceBox choiceBox = new ChoiceBox();
+			choiceBox.setItems(this.ids.getItems());
+			VBox box = new VBox();
+			Text text = new Text(String.format("Gate %s", i + 1));
+			Button scan = new Button("scan");
+			Button walkgate = new Button("Walk through gate");
+			Text error = new Text();
+			scan.setOnAction(event -> error.setText("pressed scan"));
+			walkgate.setOnAction(event -> error.setText("pressed walk through gate"));
+			box.getChildren().addAll(text, choiceBox, scan, walkgate, error);
+			newRoot.add(box, i,0);
+		}
+		Scene scene = new Scene(newRoot, 650, 300);
+		stage.setScene(scene);
 		stage.sizeToScene();
 		stage.show();
 	}
+
+
 
 	public GridPane startMetroSim(GridPane root, ArrayList<Integer> metroCardIds, int gates){
 		for(int i = 0; i < gates; i++){
